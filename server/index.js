b5325b05b9ad88dee06b5ea3b3ef150bb200f9af
markalterego <jesse.paukkunen@gmail.com>
2024-11-04 15:12:13 +0200

    todo part 5 done

--- server/index.js
+++ server/index.js
@@ -1,67 +1,18 @@
 import express from 'express';
 import cors from 'cors';
-import pkg from 'pg';
+import router from './routers/todoRouter.js';
 
-const port = 3001;
-const { Pool } = pkg;
+const port = process.env.PORT;
 
 const app = express();
 app.use(cors()); // allows testing from different origin
 app.use(express.json()); // allows client to send json data
 app.use(express.urlencoded({extended: false})); // use curl through url
+app.use('/', router);
 
-app.get('/', (req,res) => 
-{
-    const pool = openDb();
-
-    pool.query('select * from task', (error,result) => {
-        if (error) {
-            return res.status(500).json({error: error.message});
-        }
-        return res.status(200).json(result.rows); 
-    });
+app.use((err,req,res,next) => {
+    const statusCode = err.statusCode || 500;
+    res.status(statusCode).json({error: err.message});
 });
-
-app.post('/create', (req,res) => 
-{
-   const pool = openDb();
-   
-   pool.query('insert into task (description) values ($1) returning *',
-        [req.body.description],
-        (error,result) => {
-            if (error) {
-                return res.status(500).json({error: error.message});
-            }
-            return res.status(200).json({id: result.rows[0].id});
-        }
-   );
-});
-
-app.delete('/delete/:id', (req,res) => 
-{
-    const pool = openDb();
-    const id = parseInt(req.params.id);
-
-    pool.query('delete from task where id = $1',
-        [id],
-        (error,result) => {
-            if (error) {
-                return res.status(500).json({error: error.message});
-            }
-            return res.status(200).json({id: id});
-        }
-    );
-});
-
-const openDb = () => {
-    const pool = new Pool({
-        user: 'postgres',
-        host: 'localhost',
-        database: 'todo',
-        password: 'root',
-        port: 5432
-    });
-    return pool;
-};
 
 app.listen(port);
